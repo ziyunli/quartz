4ca1585b211e8259d89ca1d4c874156e99a5d696
Stephen Li <stephen.li@instacart.com>
2024-12-09 14:00:25 -0800

    more recent notes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,7 +31,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(
       Component.RecentNotes({
         title: "Recent Notes",
-        limit: 2,
+        limit: 5,
         filter: (f) => f.slug!.startsWith("notes/"),
         linkToMore: "notes/" as SimpleSlug,
       }),
